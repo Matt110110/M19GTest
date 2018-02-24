83615def9df3795ea7d9612bbe6adfc707b71771
Matt110110 <matruprasadchand@gmail.com>
2018-02-24 15:58:43 +0530

    js file changed

--- multiply.js
+++ multiply.js
@@ -1,9 +1,10 @@
+var x, y;
 $("document").ready(function () {
    $("#Btn").click(function (e){
     var elm = $(this);
-    var xPos = e.pageX - elm.offset().left;
-    var yPos = e.pageY - elm.offset().top;
+    x = e.pageX - elm.offset().left;
+    y = e.pageY - elm.offset().top;
 
-    console.log(xPos, yPos);
+    console.log(x, y);
    });
 });
